5ba297bfeff4fde20f4b9fe0b7c4d3e6aac4e442
hugo machado <hugoamac@live.com>
2021-12-10 00:30:02 -0300

    chore: move sample_class to sample folder

---
+++ src/sample/sample_class.ts
@@ -0,0 +1,8 @@
+/**
+ * This is sample class wrote in typescript
+ */
+export class SampleClass {
+  public async action(): Promise<string> {
+    return "hello typescript";
+  }
+}
--- src/sample_class.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This is sample class wrote in typescript
- */
-export class SampleClass {
-  public async action(): Promise<string> {
-    return "hello typescript";
-  }
-}
